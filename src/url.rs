1257c1b005aeceaf481dc9494962659b37f64dc4
Marshall Ku <marshall@kakao.com>
2024-01-12 15:19:00 +0900

    Verify url validator

--- src/url.rs
+++ src/url.rs
@@ -3,3 +3,25 @@ use url::Url;
 pub fn is_valid_url(url: &str) -> bool {
     Url::parse(url).is_ok()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_url() {
+        assert!(is_valid_url("https://www.example.com"));
+        assert!(is_valid_url("https://www.example.xyz"));
+        assert!(is_valid_url("https://example.xyz"));
+        assert!(is_valid_url("https://www.example.be"));
+        assert!(is_valid_url("https://www.com"));
+        assert!(is_valid_url("https://example.be"));
+        assert!(is_valid_url("http://localhost:8080/test"));
+    }
+
+    #[test]
+    fn test_invalid_url() {
+        assert!(!is_valid_url("https://"));
+        assert!(!is_valid_url("not_a_valid_url"));
+    }
+}
