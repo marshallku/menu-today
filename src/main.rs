de88a1ee38256c68c937e94adf76dd1e302ffaef
Marshall Ku <marshall@kakao.com>
2024-07-07 12:35:32 +0900

    Move log function to utils

--- src/main.rs
+++ src/main.rs
@@ -4,50 +4,21 @@ mod routes;
 mod utils;
 
 use api::meal::{get_meal, MealData};
-use axum::{body::Body, extract::Request};
 use routes::app::app;
+use utils::log::trace_layer_on_request;
 
 use std::{
     env,
     sync::{atomic::AtomicBool, Arc, Mutex},
 };
 use tower_http::trace::{self, TraceLayer};
-use tracing::{info, Level, Span};
+use tracing::{info, Level};
 
 #[derive(Clone)]
 pub struct AppState {
     pub cache: Arc<Mutex<MealData>>,
     pub fetch_in_progress: Arc<AtomicBool>,
     pub handlebars: Arc<handlebars::Handlebars<'static>>,
-}
-
-fn trace_layer_on_request(request: &Request<Body>, _span: &Span) {
-    let user_agent = request
-        .headers()
-        .get("user-agent")
-        .map_or("<no user-agent>", |h| {
-            h.to_str().unwrap_or("<invalid utf8>")
-        });
-
-    let referer = request
-        .headers()
-        .get("referer")
-        .and_then(|value| value.to_str().ok())
-        .unwrap_or("<no referer>");
-
-    let ip_address = request
-        .headers()
-        .get("x-forwarded-for")
-        .or_else(|| request.headers().get("x-real-ip"))
-        .and_then(|value| value.to_str().ok())
-        .unwrap_or("<no ip>");
-
-    tracing::info!(
-        "User-Agent: {:?} Referrer: {:?} IP: {:?}",
-        user_agent,
-        referer,
-        ip_address
-    )
 }
 
 #[tokio::main]
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,4 +2,5 @@ mod __tests__;
 
 pub mod cache;
 pub mod encode;
+pub mod log;
 pub mod url;
